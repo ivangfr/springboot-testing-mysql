7c48e6a52ae4fd9bba43c7c03fdac2782cc66f89
Ivan Franchin <ivan.franchin@takeaway.com>
2022-05-05 16:22:49 +0200

    add Mapping annotation to ignore some fields in mapper class

--- user-service/src/main/java/com/mycompany/userservice/mapper/UserMapper.java
+++ user-service/src/main/java/com/mycompany/userservice/mapper/UserMapper.java
@@ -5,20 +5,26 @@ import com.mycompany.userservice.dto.UpdateUserRequest;
 import com.mycompany.userservice.dto.UserResponse;
 import com.mycompany.userservice.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(
         componentModel = "spring",
-        unmappedTargetPolicy = ReportingPolicy.IGNORE,
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
 )
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "updatedOn", ignore = true)
     User toUser(CreateUserRequest createUserRequest);
 
     UserResponse toUserResponse(User user);
 
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdOn", ignore = true)
+    @Mapping(target = "updatedOn", ignore = true)
     void updateUserFromRequest(UpdateUserRequest updateUserRequest, @MappingTarget User user);
 }
